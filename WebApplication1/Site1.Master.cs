29b33d476da7f6e5caea647187f43dffff7aa931
SnowBit <dtshah24022006@gmail.com>
2025-07-16 15:50:54 +0530

    update

--- WebApplication1/Site1.Master.cs
+++ WebApplication1/Site1.Master.cs
@@ -5,13 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+// 1. The namespace MUST be "WebApp"
+namespace WebApp
 {
-    public partial class Site1 : System.Web.UI.MasterPage
+    // 2. The class name MUST be "SiteMaster"
+    public partial class SiteMaster : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // Your code here
         }
     }
 }
